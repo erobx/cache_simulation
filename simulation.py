297c51d84a4293edd2885bcbdb1d08730cd2725c
Evan Robinson <erob7856@gmail.com>
2022-12-03 21:00:28 -0500

    stuff

--- simulation.py
+++ simulation.py
@@ -1,11 +1,28 @@
+import time
+
+def main():
+    hex = read_file()
+    binary = convert_hex_to_binary(hex)
 
 
 def read_file():
     f = open("input/read01.trace", "r")
-    for i in f:
-        print(i)
+    lines = [line.split(' ') for line in f]
     f.close()
+    hex = [i[1][2:] for i in lines]
+    return hex
+    
+
+def convert_hex_to_binary(hex):
+    result = []
+    for i in hex:
+        integer = int(i, 16)
+        binary = format(integer, '0>42b')
+        result.append(binary)
+    return result
 
 
+def init_cache():
+    return
 
-read_file()
+main()
